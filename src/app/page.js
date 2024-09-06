5abad1c295c5561bc9cf73331ce5f23586e2225a
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2024-09-07 00:22:47 +0200

    Fix Gallery 4

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import dynamic from "next/dynamic"; // Add this line to import dynamic
 const Gallery = dynamic(() => import("./components/Gallery"), { ssr: false });
 import Header from "./components/Header";
 
